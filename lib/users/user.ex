c64d2378257d15c2452e0790c68eeb76ed9aa0af
Eduardo <eduardo.andrade15@hotmail.com>
2022-10-10 19:04:56 -0300

    created connection between structs

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,12 +1,13 @@
 defmodule Exlivery.Users.User do
-  @keys [:name, :email, :cpf, :age]
+  @keys [:name, :email, :cpf, :age, :address]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(name, email, cpf, age) when age >= 18 do
+  def build(name, email, cpf, age, address) when age >= 18 do
     {:ok,
      %__MODULE__{
+       address: address,
        name: name,
        email: email,
        cpf: cpf,
