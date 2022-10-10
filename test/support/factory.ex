09cdf3c07b3e475c6e5fd442fd5ebb6bc6025d5b
Eduardo <eduardo.andrade15@hotmail.com>
2022-10-10 20:47:30 -0300

    created test for item struct

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
+  alias Exlivery.Orders.Item
+  # alias Exlivery.Orders.Order
   alias Exlivery.Users.User
 
   def user_factory do
@@ -9,6 +11,15 @@ defmodule Exlivery.Factory do
       cpf: "12312312",
       email: "lordstark@gmail.com",
       name: "Eduardo"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :pizza,
+      description: "Pizza de peperoni",
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
     }
   end
 end
