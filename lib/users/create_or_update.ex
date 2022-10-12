453449c7a5897ed180a7592efff000a3d600280a
Eduardo <eduardo.andrade15@hotmail.com>
2022-10-12 18:26:08 -0300

    created user agent

---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,15 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users
+  alias Users.Agents, as: UserAgent
+  alias Users.User
+
+  def call(%User{cpf: cpf, address: address, age: age, email: email, name: name}) do
+    address
+    |> User.build(name, email, cpf, age)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+
+  defp save_user({:error, _reason} = error), do: error
+end
